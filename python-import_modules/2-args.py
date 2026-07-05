fe35087dd41b8bb91ac081f62254047ceb561c8b
Hemenseter <h.amospenda@alustudent.com>
2026-07-05 02:52:23 -0700

    Lets get it

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    count = len(argv) - 1
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+        print("{}: {}".format(1, argv[1]))
+    else:
+        print("{} arguments:".format(count))
+        i = 1
+        while i <= count:
+            print("{}: {}".format(i, argv[i]))
+            i += 1
---
+++ python-import_modules/README.md
@@ -0,0 +1,1 @@
+This is another one of my many README files 
