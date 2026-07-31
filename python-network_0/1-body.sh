986ce23ce3f4ee30cfbd8ab589bcc26acf085e40
Hemenseter <h.amospenda@alustudent.com>
2026-07-31 11:00:03 -0700

    Message

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body only for a 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -s -L -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s -L "$1"
